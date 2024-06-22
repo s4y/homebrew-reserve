e9dc97bbee1353325dddcba65a473b4a6f1a81d0
Sidney San Martín <sidney@s4y.us>
2024-06-22 15:51:47 -0400

    Create s4y/reserve tap

---
+++ Formula/reserve.rb
@@ -0,0 +1,17 @@
+class Reserve < Formula
+  desc "A live reloading web server with extra goodies."
+  homepage "https://github.com/s4y/reserve"
+  url "https://github.com/s4y/reserve/archive/refs/tags/v1.0.10.tar.gz"
+  sha256 "40c1f0202876db3c2f784ef8f867a2a66f95517df1a58f5275b63d7232679757"
+  license "Apache-2.0"
+  head "https://github.com/s4y/reserve.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "./reserve/reserve", "./reserve"
+    bin.install "reserve/reserve" => "reserve"
+  end
+
+end
+
---
+++ README.md
@@ -0,0 +1,18 @@
+# S4y Reserve
+
+## How do I install these formulae?
+
+`brew install s4y/reserve/<formula>`
+
+Or `brew tap s4y/reserve` and then `brew install <formula>`.
+
+Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
+
+```ruby
+tap "s4y/reserve"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
