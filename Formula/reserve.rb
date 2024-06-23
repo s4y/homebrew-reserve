b0207d1f5bdc512f35ae9ac332c28a086ac5db6b
Sidney San Martín <sidney@s4y.us>
2024-06-22 20:46:18 -0400

    CI fixes

--- Formula/reserve.rb
+++ Formula/reserve.rb
@@ -1,5 +1,5 @@
 class Reserve < Formula
-  desc "A live reloading web server with extra goodies."
+  desc "Live reloading web server with extra goodies"
   homepage "https://github.com/s4y/reserve"
   url "https://github.com/s4y/reserve/archive/refs/tags/v1.0.10.tar.gz"
   sha256 "40c1f0202876db3c2f784ef8f867a2a66f95517df1a58f5275b63d7232679757"
@@ -15,7 +15,6 @@ class Reserve < Formula
 
   test do
     output = shell_output(bin/"reserve --help 2>&1")
-    assert_match /^Usage of/, output
+    assert_match(/^Usage of/, output)
   end
 end
-
