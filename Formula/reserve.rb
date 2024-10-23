10b9ed82caf03ae143b246a525b06a149d702368
Sidney San Martín <111870+s4y@users.noreply.github.com>
2024-10-23 19:42:15 +0000

    reserve: update 1.0.11 bottle.

--- Formula/reserve.rb
+++ Formula/reserve.rb
@@ -8,14 +8,10 @@ class Reserve < Formula
 
   bottle do
     root_url "https://github.com/s4y/homebrew-reserve/releases/download/reserve-1.0.11"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "a81b86f1e2b6db0ab79a4d2c4622baa060c4a4b0d3f5a32a2c6a5255e71133d3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a81b86f1e2b6db0ab79a4d2c4622baa060c4a4b0d3f5a32a2c6a5255e71133d3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a81b86f1e2b6db0ab79a4d2c4622baa060c4a4b0d3f5a32a2c6a5255e71133d3"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a81b86f1e2b6db0ab79a4d2c4622baa060c4a4b0d3f5a32a2c6a5255e71133d3"
-    sha256 cellar: :any_skip_relocation, sonoma:         "06ca762d9337ab096b4a24f0463d7562d9e6da0f74bb3c3b73200e0444d47589"
-    sha256 cellar: :any_skip_relocation, ventura:        "7a44f21232af50a329f4d1c7a2f9f6832560fff61cb1a97445bf31284f75d717"
-    sha256 cellar: :any_skip_relocation, monterey:       "06ca762d9337ab096b4a24f0463d7562d9e6da0f74bb3c3b73200e0444d47589"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a0a3ed84c4c9967b6d14178f2c7aa4e089726eea25a39f0f0b5bc33c8095e821"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "a4e8eecbf946826115badb79a29fd0bf2e187f0accff9506349d6e53e4841f42"
+    sha256 cellar: :any_skip_relocation, ventura:      "42dc7ce98aa1de56ff039e9fae21a062c514aca68020a9430b03c4ac0cc94c34"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9564ae62adba3c0018a494dfce00119a5fdf75ff4f02bf7d41e586f91f6315f9"
   end
 
   depends_on "go" => :build
