6eb3c10f25c18b17da045798fe211b57362d6c17
Sidney San Martín <sidney@s4y.us>
2024-10-23 14:44:48 -0400

    1.0.11

--- Formula/reserve.rb
+++ Formula/reserve.rb
@@ -1,15 +1,21 @@
 class Reserve < Formula
   desc "Live reloading web server with extra goodies"
   homepage "https://github.com/s4y/reserve"
-  url "https://github.com/s4y/reserve/archive/refs/tags/v1.0.10.tar.gz"
+  url "https://github.com/s4y/reserve/archive/refs/tags/v1.0.11.tar.gz"
   sha256 "40c1f0202876db3c2f784ef8f867a2a66f95517df1a58f5275b63d7232679757"
   license "Apache-2.0"
   head "https://github.com/s4y/reserve.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/s4y/homebrew-reserve/releases/download/reserve-1.0.10"
+    root_url "https://github.com/s4y/homebrew-reserve/releases/download/reserve-1.0.11"
     rebuild 1
-    sha256 cellar: :any_skip_relocation, ventura:      "6639d8a1c2efb9b78452bada714e01a301ad3f8a3cc11f21406534425d7edfc7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "a81b86f1e2b6db0ab79a4d2c4622baa060c4a4b0d3f5a32a2c6a5255e71133d3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a81b86f1e2b6db0ab79a4d2c4622baa060c4a4b0d3f5a32a2c6a5255e71133d3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a81b86f1e2b6db0ab79a4d2c4622baa060c4a4b0d3f5a32a2c6a5255e71133d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a81b86f1e2b6db0ab79a4d2c4622baa060c4a4b0d3f5a32a2c6a5255e71133d3"
+    sha256 cellar: :any_skip_relocation, sonoma:         "06ca762d9337ab096b4a24f0463d7562d9e6da0f74bb3c3b73200e0444d47589"
+    sha256 cellar: :any_skip_relocation, ventura:        "06ca762d9337ab096b4a24f0463d7562d9e6da0f74bb3c3b73200e0444d47589"
+    sha256 cellar: :any_skip_relocation, monterey:       "06ca762d9337ab096b4a24f0463d7562d9e6da0f74bb3c3b73200e0444d47589"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "c8f7c0ab1c51f5bcf9b0502a27e150ac5c52a776e3ff4e0c9a9841b8dfea8994"
   end
 
