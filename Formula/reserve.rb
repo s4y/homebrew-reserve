b58b3658c82ae9eabc16e355d5826666c32e9097
Sidney San Martín <sidney@s4y.us>
2024-12-24 04:18:43 -0500

    Update reserve.rb

--- Formula/reserve.rb
+++ Formula/reserve.rb
@@ -8,7 +8,7 @@ class Reserve < Formula
 
   bottle do
     root_url "https://github.com/s4y/homebrew-reserve/releases/download/reserve-1.1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "42a402053895880571645a61de6ccd3e700cc82e93daf3e8bd233701efc67b60"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "42a402053895880571645a61de6ccd3e700cc82e93daf3e7bd233701efc67b60"
     sha256 cellar: :any_skip_relocation, ventura:      "1cfa4554d6f3d49c88f5cb5a20c9ee9e437ce0ed4f30ff638e07d3924c2192ef"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "a0c584db11716245030009f91f19c99d1f612cdb75ab4742ece3ca089ba48c7f"
   end
